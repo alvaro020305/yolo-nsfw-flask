ac02f99c5bef3f7f7fa90247122b716fbeb04a34
alvaro020305 <alvarochipoco020305@gmail.com>
2025-10-03 20:00:47 -0600

    add Dockerfile for Railway deploy

--- inference_server.py
+++ inference_server.py
@@ -72,7 +72,5 @@ def check_images_batch():
 # --------------------------------------------------------------------------
 # PASO 3: PONER EN MARCHA EL SERVIDOR
 # --------------------------------------------------------------------------
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
